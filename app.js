fab930b59923add3fc62b7a59e995f00b70d99ff
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-06-25 11:39:43 +0100

    added functionalities

---
+++ app.js
@@ -0,0 +1,58 @@
+const pwEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const lenEl = document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+";
+
+// this function is to set all the function to output random uppercase, lowercase, numbers, symbols
+function getUppercase() {
+  return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+function getLowercase() {
+  return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+}
+function getNumber() {
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+function getSymbol() {
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+// function to generate password
+
+function generatePassword() {
+  const len = lenEl.value;
+  let password = "";
+  for (let i = 0; i < len; i++) {
+    const x = generateX();
+    password += x;
+  }
+  pwEl.innerHTML = password;
+}
+
+// generate all the required
+function generateX() {
+  const xs = [];
+  if (upperEl.checked) {
+    xs.push(getUppercase());
+  }
+  if (lowerEl.checked) {
+    xs.push(getLowercase());
+  }
+  if (numberEl.checked) {
+    xs.push(getNumber());
+  }
+  if (symbolEl.checked) {
+    xs.push(getSymbol());
+  }
+  return xs[Math.floor(Math.random() * xs.length)];
+}
+
+// the user click and generate password
+generateEl.addEventListener("click", generatePassword);
